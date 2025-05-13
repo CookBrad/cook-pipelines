76258c128485955341b5372859f7ed6c571ea49c
Brad Cook <cook.brada@gmail.com>
2025-05-13 11:17:41 -0500

    add pipeline

---
+++ app.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+const app = new cdk.App();
+
+const pipeline = new pipelines.CodePipeline(app, 'MyPipeline', {
+    synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.connection('CookBrad/investment-calculator-ts', 'main', {
+            connectionArn: `arn:aws:codestar-connections:ueast-2:850502434229:connection/${ssm.StringParameter.valueForStringParameter(app, '/connectionArn')}`,
+        }),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+    }),
+});
