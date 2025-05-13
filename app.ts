a260a8c06013607e944a7a2434de7caa91002efe
Brad Cook <cook.brada@gmail.com>
2025-05-13 11:23:49 -0500

    add stack

--- app.ts
+++ app.ts
@@ -4,11 +4,18 @@ import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 const app = new cdk.App();
 
-const pipeline = new pipelines.CodePipeline(app, 'MyPipeline', {
-    synth: new pipelines.ShellStep('Synth', {
-        input: pipelines.CodePipelineSource.connection('CookBrad/investment-calculator-ts', 'main', {
-            connectionArn: `arn:aws:codestar-connections:ueast-2:850502434229:connection/${ssm.StringParameter.valueForStringParameter(app, '/connectionArn')}`,
-        }),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
-    }),
-});
+export class MyPipelineStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new pipelines.CodePipeline(app, 'MyPipeline', {
+            synth: new pipelines.ShellStep('Synth', {
+                input: pipelines.CodePipelineSource.connection('CookBrad/investment-calculator-ts', 'main', {
+                    connectionArn: `arn:aws:codestar-connections:ueast-2:850502434229:connection/${ssm.StringParameter.valueForStringParameter(app, '/connectionArn')}`,
+                }),
+                commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+            }),
+        });
+    }
+}
+
