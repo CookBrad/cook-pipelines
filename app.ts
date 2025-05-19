2f9c91fd9c4beee6ff4397d971e7b0432156124f
Brad Cook <cook.brada@gmail.com>
2025-05-19 11:32:37 -0500

    change connection arn

--- app.ts
+++ app.ts
@@ -12,7 +12,12 @@ export class PipelineStack extends cdk.Stack {
             synth: new pipelines.ShellStep('Synth', {
                 input: pipelines.CodePipelineSource.connection('CookBrad/investment-calculator-ts', 'main', {
                     triggerOnPush: true,
-                    connectionArn: `arn:aws:codestar-connections:us-east-2:850502434229:connection/${ssm.StringParameter.valueForStringParameter(this, '/connectionArn')}`,
+                    connectionArn: `arn:aws:codestar-connections:us-east-2:850502434229:connection/${ssm.StringParameter.fromStringParameterName(
+                        this,
+                        'connectionArn',
+                        '/connectionArn'
+                    ).stringValue})
+            }`,
                 }),
                 commands: ['npm ci', 'npm run build', 'npx cdk synth'],
             }),
